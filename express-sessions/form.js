f672ee818042bcbddd7f4a9cede988a178eeab4f
mendojen <mendojen@onid.oregonstate.edu>
2015-11-22 16:55:16 -0800

    Update form.js

--- express-sessions/form.js
+++ express-sessions/form.js
@@ -12,7 +12,7 @@ app.use(express.static('public'));
 
 app.get('/',function(req,res,next){
   var context = {};
-  var zipcode=getelementByID(zip);
+  var zipcode=context.zip;
   res.render('form',context);
   request('http://api.openweathermap.org/data/2.5/weather?q='+ zip + '&APPID=' + credentials.owmKey, function(err, response, body){
     if(!err && response.statusCode < 400){
